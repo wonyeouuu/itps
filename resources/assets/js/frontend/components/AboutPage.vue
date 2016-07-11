9d997e5f5740592a35cc5d0ed45301ca1e777cc2
ChenHung-Yu <daniel@masterbrain.com.tw>
2016-07-11 23:02:41 +0800

    debug

--- resources/assets/js/frontend/components/AboutPage.vue
+++ resources/assets/js/frontend/components/AboutPage.vue
@@ -1,5 +1,10 @@
 <style lang="stylus" scoped>
 @import './transitions/flip.styl'
+introColor = red
+hr.split
+    margin-bottom 120px
+hr.member-intro-split
+    margin 50px 0
 div#about-container
     position fixed
     top 0
@@ -59,6 +64,58 @@ div#about-container
                     font-size 40px
                     position relative
                     top -55px
+        div.member-intro
+            div.member-intro-photo
+                color introColor
+                h1
+                    text-align center
+                    font-size 100px
+                    font-weight 700
+                    position relative
+                    top -60px
+                p
+                    text-align center
+                    font-size 25px
+                    letter-spacing 160px
+                    position relative
+                    top 100px
+                    left 80px
+            div.member-intro-text
+                width 50%
+                margin 0 auto
+                h1
+                    font-size 50px
+                    font-weight 500
+                p
+                    font-size 30px
+                    font-weight 200
+                &.align-left
+                    width 50%
+                    margin-left 10%
+            div#member-intro-photo1
+                width 85%
+                height 500px
+                margin 0 auto
+                background url('/imgs/about_us_photo1.png')
+                background-size cover
+            div#member-intro-photo2
+                width 85%
+                height 500px
+                margin 0 auto
+                background url('/imgs/about_us_photo2.png')
+                background-size cover
+                h1
+                    font-size 74px
+                p
+                    top 130px
+            div#member-intro-photo3
+                width 95%
+                height 300px
+                margin 0 auto
+                background url('/imgs/about_us_photo3.png')
+                background-size 100% 100%
+
+
 </style>
 
 <template lang='jade'>
@@ -81,7 +138,44 @@ div#about-container(v-show='aboutShow', transition='flip')
                 |dentITPS<br>
                 |<span class='mail-intro-big'>@</span><br>
                 |<span class='mail-intro-small'>gmail.com</span>
-        hr
+        hr.split
+        div.member-intro
+            div.member-intro-photo#member-intro-photo1
+                h1 Po-yen chen
+                p 陳柏諺
+            div.member-intro-text
+                h1 Leader.
+                p
+                    |born in 1990, live in Taipei,<br>
+                    |Taiwan.<br>
+                    |Dentist, Founder of we<br>
+                    |design company.<br><br>
+                    |Striving for the entry of a<br>
+                    |M.S. degree in orthodontics<br>
+                    |in the U.S.<br>
+                    |Please let me know if<br>
+                    |there's one seat left.<br><br>
+                    |eric79523@gmail.com
+            hr.member-intro-split
+            div.member-intro-photo#member-intro-photo2
+                h1 Sheng-chia huang
+                p 黃勝家
+            div.member-intro-text.align-left
+                h1 Graphical Designer.
+                p
+                    |Sheng-chia huang, born in<br>
+                    |1990, live in London,<br>
+                    |England.<br>
+                    |Freelance Graphical<br>
+                    |designer. Students from<br>
+                    |CSM<br><br>
+                    |XXxxxxXXXXXX<br>
+                    |XXxxxxXXXXXX<br>
+                    |XXxxxxXXXXXX<br>
+                    |XXxxxxXXXXXX<br><br>
+                    |j173262000@gmail.com
+            hr.member-intro-split
+            div.member-intro-photo#member-intro-photo3
 
 
 
