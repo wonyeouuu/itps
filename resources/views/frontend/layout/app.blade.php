2594e8999edd236ba1b6f0ee7e3effee944752b6
ChenHung-Yu <daniel@masterbrain.com.tw>
2016-10-14 03:56:12 +0800

    add meta tag for mobile

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <title>ITPS</title>
+        <meta name='viewport' content='width=device-width,initial-scale=1.0'>
         <link rel="stylesheet" href="{{ elixir('css/frontend/bootstrap.css') }}">
         @yield('styles.header')
     </head>
